6115d46c6bec92f1b5e6af791d12b155396784ae
Eyyüp Açık <eacik@outlook.com.tr>
2023-08-12 17:21:43 +0300

    FirstComment

---
+++ app.js
@@ -0,0 +1,17 @@
+self.addEventListener("load", async () => {
+  console.log("sayfa yüklendi");
+  const sW = await navigator.serviceWorker.register("/sw.js");
+  console.log("Service Worker => ", sW);
+
+  const subscribeButton = document.querySelector("#subscribe");
+  subscribeButton.addEventListener("click", async () => {
+    const serviceWorker = await navigator.serviceWorker.ready;
+    const clientID = await serviceWorker.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey:
+        "BEY6ts-VhjTHQbMRQ8qTSijhDfQuF3VZtPV5gxSjOrvX5iIp6m0DsGF9qvTm24FWxwMkR3rNi69Zu_xfwDSPNiM",
+    });
+    console.log(clientID);
+    console.log(JSON.stringify(clientID));
+  });
+});
